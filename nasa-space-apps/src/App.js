410fe1543a1c2f010a6aa66234b15606c8991fd1
Nicholas <nicholas.pucharelli@gmail.com>
2025-10-04 15:19:16 -0300

    movendo Home para um component proprio e criando a pasta components

--- nasa-space-apps/src/App.js
+++ nasa-space-apps/src/App.js
@@ -2,43 +2,7 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import ThreeDemo from './ThreeDemo';
-
-import LPMeteor from './lpmeteor';
-
-function Home() {
-  const [offset, setOffset] = React.useState({ x: 0, y: 0 });
-
-  const handleMouseMove = React.useCallback((e) => {
-    const { innerWidth: w, innerHeight: h } = window;
-    const x = ((e.clientX / w) - 0.5) * 2;
-    const y = ((e.clientY / h) - 0.5) * 2;
-    setOffset({ x, y });
-  }, []);
-
-  const translate = (m) => `translate3d(${offset.x * m}px, ${offset.y * m}px, 0)`;
-
-  return (  
-    <div className="scene" style={{
-    }} onMouseMove={handleMouseMove}>
-      {/* Parallax layers */}
-      <div className="" style={{ transform: translate(5) }} />
-      <div className="layer layer-far" style={{ 
-      backgroundImage: "url('/earth_landing_page.jpg')",
-        backgroundSize: "cover",      // cobre toda a área
-        backgroundPosition: "center", // centraliza a imagem
-        backgroundRepeat: "no-repeat", transform: translate(10) }} />
-      {/* <div className="layer layer-mid" style={{ transform: translate(20) }} /> */}
-      {/* <div className="layer layer-near" style={{ transform: translate(35) }} /> */}
-
-      {/* Content */}
-      <LPMeteor />
-      <div className="content">
-        <h1>Welcome to the space experience</h1>
-        <Link to="/ThreeDemo" className="cta">Open 3D Model</Link>
-      </div>
-    </div>
-  );
-}
+import Home from './components/Home';
 
 export default function App() {
   return (
